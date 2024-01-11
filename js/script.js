f3e3bf676807209170ae89b4d4bfc977253e475d
ParaBalaraman <pbal978@aucklanduni.ac.nz>
2024-01-11 18:11:30 +1300

    Dropdown menu for RSVP

--- js/script.js
+++ js/script.js
@@ -84,6 +84,11 @@ $(document).ready(function(){
         }
     }
 
+    mobiscroll.select('#multiple-select', {
+        inputElement: document.getElementById('my-input'),
+        touchUi: false
+    });
+
 });
 
 
